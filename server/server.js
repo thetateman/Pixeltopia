2686c83a776830af19496723413669c1cbe615d2
thetateman <tdsmith2018@gmail.com>
2024-08-02 19:50:32 -0500

    updates

--- server/server.js
+++ server/server.js
@@ -1,37 +1,37 @@
 "use strict";
 
-const path = require('path');
-const http = require('http');
-const express = require('express');
-const helmet = require('helmet');
-const { WebSocketServer, WebSocket } = require('ws');
+const path = require("path");
+const http = require("http");
+const express = require("express");
+const helmet = require("helmet");
+const { WebSocketServer, WebSocket } = require("ws");
 const rateLimit = require("ws-rate-limit");
-
 
 const app = express();
 
 // Security middleware
-app.set('trust proxy', 'loopback');
+// app.set("trust proxy", "loopback");
 
-app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-    directives: {
-        connectSrc: ["'self'", "wss://pixeltopia.fun"], //TODO: Replace
-    },
-}));
+// app.use(helmet());
+// app.use(helmet.contentSecurityPolicy({
+//     directives: {
+//         connectSrc: ["'self'", "wss://pixeltopia.fun"], //TODO: Replace
+//     },
+// }));
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-
-
+app.use(express.urlencoded({ extended: true }));
 
 //app.use("/api", apiRouter);
 app.get("/robots.txt", (req, res) => {
-    res.sendFile(path.resolve(`${__dirname}/../robots.txt`));
+  res.sendFile(path.resolve(`${__dirname}/../robots.txt`));
 });
 
-
-app.use(express.static(path.resolve(`${__dirname}/../client`), {index: 'index.html'}));
+app.use(
+  express.static(path.resolve(`${__dirname}/../client`), {
+    index: "index.html",
+  })
+);
 
 /*
 
@@ -51,11 +51,11 @@ app.use('/game', (req, res) => {
 */
 
 const server = http.createServer(app);
-let wss = new WebSocketServer({server, maxPayload: 1024 * 4});
+let wss = new WebSocketServer({ server, maxPayload: 1024 * 4 });
 const wsClients = {};
 const wsSubscribers = {
   newToken: [],
-  local: []
+  local: [],
 };
 
 let myRateLimit = rateLimit("1s", 8);
@@ -65,96 +65,96 @@ function heartbeat() {
 let addressToClientMap = {};
 let rateLimitOffenses = {};
 let rateLimitMsgSizeOffenses = {};
-wss.on('connection', function connection(ws, req){
-    console.log("connected")
-    let ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    ws.ip_address = ip;
-    if(!addressToClientMap.hasOwnProperty(ip)){
-      addressToClientMap[ip] = 0;
+wss.on("connection", function connection(ws, req) {
+  console.log("connected");
+  let ip = req.headers["x-forwarded-for"] || req.socket.remoteAddress;
+  ws.ip_address = ip;
+  if (!addressToClientMap.hasOwnProperty(ip)) {
+    addressToClientMap[ip] = 0;
+  }
+  addressToClientMap[ip]++;
+  if (addressToClientMap[ip] > 3) {
+    addressToClientMap[ip]--;
+    console.log("too many ws connections" + ip);
+    if (!rateLimitOffenses.hasOwnProperty(ip)) {
+      rateLimitOffenses[ip] = 0;
     }
-    addressToClientMap[ip]++;
-    if(addressToClientMap[ip] > 3){
-      addressToClientMap[ip]--;
-      console.log("too many ws connections" + ip);
-      if(!rateLimitOffenses.hasOwnProperty(ip)){
-        rateLimitOffenses[ip] = 0;
-      }
-      rateLimitOffenses[ip]++;
-      if (rateLimitOffenses[ip] > 15){
-        banIP(ip);
-      } else {
-          setTimeout(()=>{
-            if(rateLimitOffenses[ip] > 0){
-              rateLimitOffenses[ip]--;
-            }
-          }, 1000 * 60 * 15);
-      }
-      ws.terminate();
-      return;
+    rateLimitOffenses[ip]++;
+    if (rateLimitOffenses[ip] > 15) {
+      banIP(ip);
     } else {
-      // setTimeout(()=>{
-      //   if(addressToClientMap[ip] > 0){
-      //     addressToClientMap[ip]--;
-      //   }
-      // }, 1000 * 60 * 5);
+      setTimeout(() => {
+        if (rateLimitOffenses[ip] > 0) {
+          rateLimitOffenses[ip]--;
+        }
+      }, 1000 * 60 * 15);
     }
-    myRateLimit(ws);
-    ws.on('limited', data => {
-      console.log("limited" + ip);
-    })
-    ws.on('pong', heartbeat);
-    ws.id = crypto.randomUUID();
-    wsClients[ws.id] = ws;
-    ws.on('error', (e)=>{
-      console.error(`WS message exceeded max size. IP: ${ws.ip_address} Time: ${Date.now()}`);
-      if(!rateLimitMsgSizeOffenses.hasOwnProperty(ws.ip_address)){
-        rateLimitMsgSizeOffenses[ws.ip_address] = 0;
-      }
-      rateLimitMsgSizeOffenses[ws.ip_address]++;
-      if (rateLimitMsgSizeOffenses[ws.ip_address] > 35){
-        banIP(ws.ip_address);
+    ws.terminate();
+    return;
+  } else {
+    // setTimeout(()=>{
+    //   if(addressToClientMap[ip] > 0){
+    //     addressToClientMap[ip]--;
+    //   }
+    // }, 1000 * 60 * 5);
+  }
+  myRateLimit(ws);
+  ws.on("limited", (data) => {
+    console.log("limited" + ip);
+  });
+  ws.on("pong", heartbeat);
+  ws.id = crypto.randomUUID();
+  wsClients[ws.id] = ws;
+  ws.on("error", (e) => {
+    console.error(
+      `WS message exceeded max size. IP: ${ws.ip_address} Time: ${Date.now()}`
+    );
+    if (!rateLimitMsgSizeOffenses.hasOwnProperty(ws.ip_address)) {
+      rateLimitMsgSizeOffenses[ws.ip_address] = 0;
+    }
+    rateLimitMsgSizeOffenses[ws.ip_address]++;
+    if (rateLimitMsgSizeOffenses[ws.ip_address] > 35) {
+      banIP(ws.ip_address);
+    }
+  });
+  ws.on("close", function close(message) {
+    if (addressToClientMap[ip] > 0) {
+      addressToClientMap[ip]--;
+    }
+  });
+  ws.on("message", function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
       }
     });
-    ws.on('close', function close(message){
-      if(addressToClientMap[ip] > 0){
+    const message = isBinary ? data : data.toString();
+    console.log(message);
+  });
+});
+const interval = setInterval(function ping() {
+  wss.clients.forEach(function each(ws) {
+    if (ws.isAlive === false) {
+      let ip = ws.ip_address;
+      if (addressToClientMap[ip] > 0) {
         addressToClientMap[ip]--;
       }
-    })
-    ws.on('message', function message(data, isBinary){
-        wss.clients.forEach(function each(client) {
-            if (client.readyState === WebSocket.OPEN) {
-            client.send(data, { binary: isBinary });
-            }
-        });
-      const message = isBinary ? data : data.toString();
-      console.log(message);
-    })
+      return ws.terminate();
+    }
+
+    ws.isAlive = false;
+    ws.ping();
   });
-  const interval = setInterval(function ping() {
-    wss.clients.forEach(function each(ws) {
-      if (ws.isAlive === false) {
-        let ip = ws.ip_address;
-        if(addressToClientMap[ip] > 0){
-          addressToClientMap[ip]--;
-        }
-        return ws.terminate()
-      };
-  
-      ws.isAlive = false;
-      ws.ping();
-    });
-  }, 25000);
-  
-  wss.on('close', function close() {
-    clearInterval(interval);
-  });
+}, 25000);
 
+wss.on("close", function close() {
+  clearInterval(interval);
+});
 
-
-server.on('error', (err) => {
-    console.error(err);
+server.on("error", (err) => {
+  console.error(err);
 });
 
 server.listen(8080, () => {
-    console.log('server started');
+  console.log("server started");
 });
